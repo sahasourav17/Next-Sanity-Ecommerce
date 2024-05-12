c0aaea1e2cb57f1c40415228b347de6903176bef
sahasourav17 <sahasourav1170@gmail.com>
2024-05-12 23:19:03 +0600

    init setup and configure sanity

---
+++ src/app/(admin)/studio/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Studio from "./Studio";
+
+const SanityPage = () => {
+  return <Studio />;
+};
+
+export default SanityPage;
---
+++ src/components/Banner.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia minima
+        quisquam exercitationem vel fugiat est, saepe repellat accusamus nobis.
+        Aliquid quos molestias ipsam maiores, qui nulla! Laudantium quae et
+        ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
+        sint molestias fugiat temporibus nihil exercitationem iure. Maxime culpa
+        magni est quas unde esse, vitae eum fuga quaerat minus modi earum.
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
