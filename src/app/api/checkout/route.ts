548c698831ab7442b1f2fbbd7109b4d5a34bf46f
sahasourav17 <sahasourav1170@gmail.com>
2024-05-21 10:49:13 +0600

    fix: stripe success url

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -27,8 +27,7 @@ export const POST = async (request: NextRequest) => {
       payment_method_types: ["card"],
       line_items: updatedItems,
       mode: "payment",
-      success_url:
-        "http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXTAUTH_URL}/cancel`,
       metadata: {
         email,
